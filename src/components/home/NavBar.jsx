bb1971e9fe827d6d9e5e5f8e360b83d952e76d86
imanee11 <imaneelouannane@gmail.com>
2025-06-13 16:42:47 +0100

    add the folders

---
+++ src/components/home/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <>
+            <h1 className='bg-red-400'>HII</h1>
+            
+        </>
+    );
+};
+
+export default NavBar;
